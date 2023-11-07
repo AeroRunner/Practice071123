123cb0b114962b336286e162e962d2bae35cd456
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-07 12:38:07 +0300

    init commit

---
+++ src/test/java/Practice.java
@@ -0,0 +1,17 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Practice {
+    @BeforeAll
+    static void configAll(){
+        Configuration.browserSize = "1920x1080";
+        }
+        @Test
+        void training1(){
+        open("https://www.google.com/");
+        }
+    }
+
